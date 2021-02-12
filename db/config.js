be4f26af30994a57bbdf6fbaea8de83f105781da
Carolina R <carolinarizk@gmail.com>
2021-02-12 18:10:28 -0500

    removed pers info

--- db/config.js
+++ db/config.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
   user: 'root',
 
   // Be sure to update with your own MySQL password!
-  password: '1234qwer',
+  password: '',
   database: 'employee_db',
 });
 module.exports = connection;
