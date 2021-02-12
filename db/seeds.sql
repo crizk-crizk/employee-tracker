4ef88a2fcf981111f007a1d44cebbbffcae40bab
Carolina R <carolinarizk@gmail.com>
2021-02-12 17:42:55 -0500

    added seed file content

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,42 @@
--- seeds--
-INSERT INTO products (flavor, price, quantity)
-VALUES ("vanilla", 2.50, 100);
+--department seeds--
+INSERT INTO department (name)
+VALUES ("marketing");
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("chocolate", 3.10, 120);
+INSERT INTO department (name)
+VALUES ("graphics");
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("strawberry", 3.25, 75);
+INSERT INTO department (name)
+VALUES ("r&d");
 
-INSERT INTO products 
-  (flavor, price, quantity)
-VALUES
-  ("mango", 3.12, 105),
-  ("banana", 3.32, 111),
-  ("caramel", 3.11, 121),
-  ("mint", 3.22, 66),
-  ("rainbow", 3.43, 122);
+INSERT INTO department (name)
+VALUES ("engineering");
+
+--department seeds--
+INSERT INTO role (title, salary, department_id)
+VALUES ("Copy Writer", 50000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("UX designer", 75000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Analyst", 180000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Eng", 120000, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Team Lead", 180000, 4);
+
+--employee seeds--
+-- team lead
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Ana", "Reese", 5);
+-- UX guy
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Max", "Smith", 2, 1);
+--analyst
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Bob", "Spoon", 3);
+--software eng.
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jim", "Kaplan", 4, 1);
