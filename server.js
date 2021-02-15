bc2f4fa0a11c9059f7f42d5b00207b5a834a0f31
Carolina R <carolinarizk@gmail.com>
2021-02-15 05:03:15 -0500

    removed config info

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
 
-// inquirer prompts
+// // inquirer prompts
 
-// **Build a command-line application that at a minimum allows the user to:
+// // **Build a command-line application that at a minimum allows the user to:
 
-  // 1) Add departments, roles, employees
+//   // 1) Add departments, roles, employees
 
-  // 2) View departments, roles, employees
+//   // 2) View departments, roles, employees
 
-  // 3) Update employee roles
+//   // 3) Update employee roles
 
-// **Bonus points if you're able to:
+// // **Bonus points if you're able to:
 
-// bonus 1) Update employee managers
-// bonus 2) inquirer prompts
-inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "whatToDo",
-        message: "Who is the employee's manager?",
-        choices: [
-          // come from name input by user
-          "", 
-          "", 
-          ""
-        ],
-      },
-    ])
+// // bonus 1) Update employee managers
+// // bonus 2) inquirer prompts
+// inquirer
+//     .prompt([
+//       {
+//         type: "list",
+//         name: "whatToDo",
+//         message: "Who is the employee's manager?",
+//         choices: [
+//           // come from name input by user
+//           "", 
+//           "", 
+//           ""
+//         ],
+//       },
+//     ])
 
-  // View employees by manager
+//   // View employees by manager
 
-  // Delete departments, roles, and employees
+//   // Delete departments, roles, and employees
 
-  // View the total utilized budget of a department -- ie the combined salaries of all employees in that department
+//   // View the total utilized budget of a department -- ie the combined salaries of all employees in that department
 
 
-  //import controller functions - call after .then
+//   //import controller functions - call after .then
