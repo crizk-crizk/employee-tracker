c43a5b2309f4a3c1acf639cf6021ab1d144e4117
Carolina R <carolinarizk@gmail.com>
2021-02-16 17:29:11 -0500

    required dependencies and called starting prompt

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// //importing
-// const connection = require("./db/config.js");
-// const prompt = require('./prompts.js')
+require('dotenv').config()
 
-// // 1st: Connect to the DB
-// const getAllRecords = (table) => {
-//   const queryString = `SELECT * FROM ${table}`
-//   connection.query(queryString, (err, res) => {
-//     if (err) throw err;
-//     //log all results of the SELECT statement
-//     console.table(res);
-//     prompt.promptWhatToDo();
-//   })
-// }
-// getAllRecords('employee');
+//importing
+const connection = require("./db/config.js");
+const {
+  startingPrompt
+} = require('./startingPrompt.js')
+
+const {
+  getAllRecords
+} = require("./db/controllers.js");
+
+startingPrompt();
+
+
