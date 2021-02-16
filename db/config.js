b10f3363c7a50ebc76d1aa57649de9ca8d0dc6cd
Carolina R <carolinarizk@gmail.com>
2021-02-16 06:22:42 -0500

    added the starting prompt

--- db/config.js
+++ db/config.js
@@ -2,15 +2,13 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-
   // Your port; if not 3306
   port: 3306,
-
   // Your username
   user: 'root',
-
   // Be sure to update with your own MySQL password!
-  password: '',
+  password: '1234qwer',
   database: 'employee_db',
 });
+
 module.exports = connection;
